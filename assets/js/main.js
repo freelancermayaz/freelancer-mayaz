bf066e6cb6beba61959eb93d2fd50b02e70d2ce2
Freelancer Mayaz <mdmayaz2002@gmail.com>
2025-03-17 23:42:05 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,74 @@
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+   const toggle = document.getElementById(toggleId),
+         nav = document.getElementById(navId)
+
+   toggle.addEventListener('click', () =>{
+       // Add show-menu class to nav menu
+       nav.classList.toggle('show-menu')
+
+       // Add show-icon to show and hide the menu icon
+       toggle.classList.toggle('show-icon')
+   })
+}
+
+showMenu('nav-toggle','nav-menu')
+
+
+const text = document.querySelector(".sec-text");
+
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = "Web Devoleper";
+    }, 0);
+    setTimeout(() => {
+        text.textContent = "Web Digainer";
+    }, 4000);
+    setTimeout(() => {
+        text.textContent = "Grafix Digainer";
+    }, 8020);
+    setTimeout(() => {
+        text.textContent = "Digital Marketer";
+    }, 12040); //1s = 1000 milliseconds
+}
+
+textLoad();
+setInterval(textLoad, 16000);
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
+
+
+
